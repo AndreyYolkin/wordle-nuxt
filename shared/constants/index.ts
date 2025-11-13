8b413a49510bf105a6aeb4c36757692b6906a5bc
Andrei Elkin <andreyyolkin@gmail.com>
2025-11-13 17:45:14 +0100

    feat(constants): add LETTERS_COUNT constant for word length

--- shared/constants/index.ts
+++ shared/constants/index.ts
@@ -1,6 +1,7 @@
 // Game constants
 export const DEFAULT_LOCALE = 'ru-RU'
 export const DEFAULT_MAX_ROWS = 6
+export const LETTERS_COUNT = 5
 
 // Timezone constants
 export const TIMEZONE = 'Europe/Warsaw'
