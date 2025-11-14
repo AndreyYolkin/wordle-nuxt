58bb712df838a9a977802939c1ace60a0a2b1be9
Andrei Elkin <andreyyolkin@gmail.com>
2025-11-14 11:47:49 +0100

    refactor: comment out unused Auth component

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="min-h-screen flex flex-col items-center justify-start p-6 gap-6 bg-slate-50 dark:bg-slate-900">
     <NuxtRouteAnnouncer />
+    <!-- <Auth /> -->
     <NuxtPage />
     <Toaster />
   </div>
