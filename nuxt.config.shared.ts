98f5dd4cd59719beada3b916c3380dacf9d8c53c
Andrei Elkin <andreyyolkin@gmail.com>
2025-11-14 11:27:58 +0100

    feat: add client and server and deps

---
+++ nuxt.config.shared.ts
@@ -0,0 +1,14 @@
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  modules: ['@unocss/nuxt', 'nuxt-auth-utils'],
+  devtools: { enabled: true },
+  nitro: {
+    experimental: {
+      tasks: true,
+      openAPI: true,
+    },
+  },
+  experimental: {
+    typedPages: true,
+  },
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,21 @@
-export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  modules: ['@unocss/nuxt', 'nuxt-auth-utils'],
-  devtools: { enabled: true },
-  nitro: {
-    experimental: {
-      tasks: true,
-      openAPI: true,
-    },
-  },
-  experimental: {
-    typedPages: true,
-  },
-})
+import clientConfig from './nuxt.config.client'
+import serverConfig from './nuxt.config.server'
+import sharedConfig from './nuxt.config.shared'
+
+const buildFormat = process.env.BUILD_FORMAT || 'fullstack'
+
+const config = (() => {
+  switch (buildFormat) {
+    case 'server': {
+      return serverConfig
+    }
+    case 'client': {
+      return clientConfig
+    }
+    default: {
+      return sharedConfig
+    }
+  }
+})()
+
+export default defineNuxtConfig(config)
