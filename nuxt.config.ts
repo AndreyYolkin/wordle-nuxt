cc0ddb2dc3bca8b9d33bef9776e98148c8f6f2a8
Andrei Elkin <andreyyolkin@gmail.com>
2025-11-18 19:32:16 +0300

    docs: add .env.example file with auth configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  // TODO: Add nuxt-auth-utils
+  // TODO: show ens
   modules: ['@unocss/nuxt'],
   devtools: { enabled: true },
   experimental: {
