b99efe38e70ba11a9b403053af66c3483f640992
Andrei Elkin <andreyyolkin@gmail.com>
2025-11-18 19:05:21 +0300

    docs(schemas): add todo comment for roomId in guess schema

--- shared/schemas/guess.ts
+++ shared/schemas/guess.ts
@@ -2,6 +2,7 @@ import * as v from 'valibot'
 import { LETTERS_COUNT } from '../constants'
 
 export const createGuessSchema = v.object({
+  // TODO add roomId
   word: v.pipe(v.string(), v.length(LETTERS_COUNT)),
 })
 
