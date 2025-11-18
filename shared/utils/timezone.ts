e9ff31ba9368358ef3751f49bbe984f5cb689146
Andrei Elkin <andreyyolkin@gmail.com>
2025-11-18 16:39:15 +0300

    refactor(timezone): simplify return type declarations for timezone utils

--- shared/utils/timezone.ts
+++ shared/utils/timezone.ts
@@ -2,24 +2,24 @@ import { DateTime } from 'luxon'
 import { TIMEZONE } from '../constants'
 import { WORD_COUNT } from './word'
 
-export const getTZTime = (): DateTime => DateTime.now().setZone(TIMEZONE)
+export const getTZTime = (): DateTime<true> => DateTime.now().setZone(TIMEZONE) as DateTime<true>
 
-export const getStartOfDay = (): DateTime => getTZTime().startOf('day')
+export const getStartOfDay = () => getTZTime().startOf('day')
 
-export const getTomorrowStartOfDay = (): DateTime => getTZTime().plus({ days: 1 }).startOf('day')
+export const getTomorrowStartOfDay = () => getTZTime().plus({ days: 1 }).startOf('day')
 
-export const getTodaysIndex = (): number => getTZTime().startOf('day').toSeconds() % WORD_COUNT
+export const getTodaysIndex = () => getTZTime().startOf('day').toSeconds() % WORD_COUNT
 
-export const getWarsawDateKey = (): Date => getTZTime().toJSDate()
+export const getWarsawDateKey = () => getTZTime().toJSDate()
 
-export function getSecondsUntilNextDay (): number {
+export function getSecondsUntilNextDay () {
   const now = getTZTime()
   const tomorrow = now.plus({ days: 1 }).startOf('day')
   const diff = tomorrow.diff(now, ['seconds'])
   return Math.max(0, Math.floor(diff.seconds))
 }
 
-export function getTimeUntilNextDay (): { isOver: boolean, hours: number, minutes: number, seconds: number } {
+export function getTimeUntilNextDay () {
   const now = getTZTime()
   const tomorrow = now.plus({ days: 1 }).startOf('day')
   const diff = tomorrow.diff(now, ['hours', 'minutes', 'seconds'])
