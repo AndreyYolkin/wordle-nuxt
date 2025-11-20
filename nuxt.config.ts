b3f33d2d6ee48893fb8aa830edaaead8464ab36b
Andrei Elkin <andreyyolkin@gmail.com>
2025-11-20 12:05:26 +0300

    refactor: remove vue-sonner imports and add as nuxt module
    Remove direct imports of vue-sonner from app.vue and add it as a nuxt module in nuxt.config.ts for better dependency management

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   // TODO: Add nuxt-auth-utils
   // TODO: show ens
-  modules: ['@unocss/nuxt'],
+  modules: ['vue-sonner/nuxt', '@unocss/nuxt'],
   devtools: { enabled: true },
   experimental: {
     typedPages: true,
